08bc2aae66cf95cd17d8a84ad3a95ef6e7419b89
Won-Kee Hong <wkhong89@gmail.com>
2019-02-28 22:16:17 +0900

    Initial commit

---
+++ 01.cond_loop/lec1_6.java
@@ -0,0 +1,21 @@
+/** 
+ *  인사말 출력하기
+ *
+ *  현재시간은 Thu Aug 06 10:15:27 KST 2019
+ *  Good Morning
+ *
+ *  현재시간은 Thu Aug 06 22:15:27 KST 2019
+ *  Good Evening
+ *
+ *  << Date 클래스 >>
+ *  << LocalDateTime 클래스 >>
+ */
+
+// 1. 클래스 이름: Welcome
+// 2. main 메소드
+// 3. 변수
+//   - date: Date 클래스 객체
+//   - currentHour: 현재 시간
+// 4. 현재 시간 출력
+// 5. 조건문: Good morning/Good afternoon/Good Evening/ Good Night 출력
+
---
+++ 01.cond_loop/lec1_7.java
@@ -0,0 +1,17 @@
+/**
+ *  Switch 문 사용하여 성적을 학점으로 변환하기
+ *
+ *  성적을 입력하시오: [92]
+ *  학점: A
+ */
+
+// 1. 클래스 이름: Score2Grade
+// 2. main 메소드
+// 3. 변수
+//    - score: 입력받은 성적
+//    - grade: 학점
+//    - scoreDivdeBy10
+//    - input: Scanner 객체
+// 4. Switch/Case를 이용하여 학점 계산
+// 5. 계산된 학점 출력
+//
